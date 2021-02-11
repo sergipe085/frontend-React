2c95eb5289c5dc424f19133bde7d6ec44f0f8f79
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-11 19:33:53 -0300

    React components study

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+ 
+const App = () => {
+  return(
+    //fragment
+    <>
+      <Header></Header>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App.js';
 
-render(<h1>Hello World</h1>, document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
