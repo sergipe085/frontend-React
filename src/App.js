2eb168c08c7195a5625706db3e05e2f60837f70e
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-12 09:59:51 -0300

    add&list projects on front end (withouth insomnia)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header';
 const App = () => {
   //retorna um array com 2 valores. 1 - a variavel 2 - um metodo para mudar o valor dessa variavel
   const [ projects, setProjects ] = useState([]);
+  const [ inputText, setInput ] = useState("");
 
   useEffect(() => {
     api.get('/projects').then(response => {
@@ -18,6 +19,22 @@ const App = () => {
 
   function handleAddProject() {
     //setProject([...projects, `Novo Projeto ${Date.now()}`]); //imutabilidade
+    //const res = await api.post('/projects');
+    //console.log(res);
+    api.post('/projects', 
+    { 
+      "title": inputText,
+      "url": "https://github.com/serjolas1",
+      "owner": "Serjolas"
+    }).then((response) => {
+      setProjects([...projects, response.data]);
+      
+    });
+  }
+
+  function onInputChanged(event) {
+    setInput(event.target.value);
+    console.log(inputText);
   }
 
   return(
@@ -29,6 +46,7 @@ const App = () => {
         {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
+      <input type="text" onChange={onInputChanged}></input>
       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
     </>
   );
