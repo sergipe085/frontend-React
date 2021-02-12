a1a91e51b1b16e6b035fca0aa2d2941bd0a76aa6
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-11 22:29:12 -0300

    Front-end & Back-end connection with axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
-import backgroundImage from './assets/backgroundImage.jfif';
+import api from './services/api.js';
 
 import Header from './components/Header';
  
 const App = () => {
   //retorna um array com 2 valores. 1 - a variavel 2 - um metodo para mudar o valor dessa variavel
-  const [ projects, setProject ] = useState(["Jogo de Terror", "Jogo de Fazer Pão"]);
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    })
+  }, [])
 
   function handleAddProject() {
-    setProject([...projects, `Novo Projeto ${Date.now()}`]); //imutabilidade
+    //setProject([...projects, `Novo Projeto ${Date.now()}`]); //imutabilidade
   }
 
   return(
     //fragment
     <>
-      <img src={backgroundImage} width="300" height="300"></img>
-
       <Header title="Projects"/>
 
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
