a9e6ea4108b09d060c6554a67ee715a83a0f2bed
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-11 21:10:57 -0300

    State and Immutability

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
  
 const App = () => {
+  //retorna um array com 2 valores. 1 - a variavel 2 - um metodo para mudar o valor dessa variavel
+  const [ projects, setProject ] = useState(["Jogo de Terror", "Jogo de Fazer Pão"]);
+
+  function handleAddProject() {
+    setProject([...projects, `Novo Projeto ${Date.now()}`]); //imutabilidade
+  }
+
   return(
     //fragment
     <>
-      <Header title="Pao">
-        <ul>
-          <li>Menu</li>
-          <li>Projetos</li>
-          <li>Login</li>
-        </ul>
-      </Header>
-      <Header title="com Quejo"/>
+      <Header title="Projects"/>
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
     </>
   );
 }
